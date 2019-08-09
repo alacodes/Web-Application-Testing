a512fe171a5742997fe2d3dc8a2ded6985a9218d
Tracie T <traciecodes@gmail.com>
2019-08-09 09:54:48 -0500

    5th: Added functionality to app.js, created test for all buttons

--- web-app-testing/src/App.js
+++ web-app-testing/src/App.js
@@ -1,15 +1,48 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Display from './components/Display'
 import Controls from './components/Dashboard'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App"> 
-        <Display strikes={2} balls={3}/> 
-        <Controls strike={()=> console.log('strike')}/>
-      </div>
-    );
-  }
-}export default App;
+
+
+function App() {
+  const [strikes, setStrikes] = useState(0)
+  const [balls, setBalls] = useState(0)
+
+  const strikeAdd = () => {
+    if (strikes === 2) {
+      setStrikes(0)
+      setBalls(0)
+    } else {
+      setStrikes(strikes+1)
+    }
+  };
+  const ballAdd = () => {
+    if (balls === 3) {
+      setStrikes(0)
+      setBalls(0)
+    } else {
+      setBalls(balls+1)
+    }
+  };
+  const foulAdd = () => {
+    if (strikes !== 2) {
+      setStrikes(strikes+1)
+    }
+  };
+  const hitAdd = () => {
+    setStrikes(0)
+    setBalls(0)
+  };
+  return(
+    <div className="App"> 
+      <Display strikes={strikes} balls={balls}/> 
+      <Controls strike={strikeAdd}
+                ball={ballAdd}
+                foul={foulAdd}
+                hit={hitAdd} />
+    </div>
+  )
+}
+
+export default App;
--- web-app-testing/src/components/Dashboard.test.js
+++ web-app-testing/src/components/Dashboard.test.js
@@ -17,32 +17,24 @@ describe('<Controls />', () => {
     //mocking function through jest
     it('strike imposter', () => {
         const click = jest.fn()
-        const { getByText } = render(<Controls strike={() => click} />)
+        const { getByText } = render(<Controls strike={click} />)
         const strikeButton = getByText(/strike/i)
         fireEvent.click(strikeButton)
         expect(click).toBeCalled()
+    })
+    //test for functionality of all buttons
+    it('confirms all buttons work', () => {
+        const buttonName = ['strike', 'ball', 'foul', 'hit']
+        const buttonFunction = buttonName.map(() => jest.fn())
+        const props = buttonName.reduce((acc, text, index) => ({...acc, [text]: buttonFunction[index]}) , {});
+        const {getByText} = render(<Controls {...props} />);
+        const buttons = buttonName.map(text => getByText(new RegExp(text, 'i')));
+        buttons.forEach((button, index) => {
+            fireEvent.click(button);
+            expect(buttonFunction[index]).toBeCalled();
+        })
     })
 })
 
 
 
-
-
-// describe('<Display />', () => {
-//     it('renders without crashing', () => {
-//         render(<Display />)
-//     });
-//     it('has props that work', () => {
-//         const balls = 3, strikes = 2;
-//         const display = render(<Display strikes={strikes} balls={balls} />)
-//         //creating dynamic regular expressions
-//         display.getByText(new RegExp(`Strikes: ${strikes}`, 'i'))
-//         display.getByText(new RegExp(`Balls: ${balls}`, 'i'))
-//     });
-//     //edge case to clarify specific behavior (want dumb components)
-//     it('handles edge cases', () => {
-//         const display = render(<Display strikes={200} balls={-10000000} />)
-//         display.getByText(new RegExp(/Strikes: 200/))
-//         display.getByText(new RegExp(/Balls: -10000000/))
-//     })
-// })
