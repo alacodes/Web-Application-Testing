17e2ddf022912047ba829c601eb75c95d0c2a4a6
Tracie T <traciecodes@gmail.com>
2019-08-09 09:16:52 -0500

    3rd: built buttons and started testing

--- web-app-testing/src/App.js
+++ web-app-testing/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Display from './components/Display'
+import Controls from './components/Dashboard'
 
 class App extends Component {
   render() {
     return (
       <div className="App"> 
-        <Display strikes={2} balls={3}/>     
+        <Display strikes={2} balls={3}/> 
+        <Controls strike={()=> console.log('strike')}/>
       </div>
     );
   }
--- web-app-testing/src/components/Dashboard.test.js
+++ web-app-testing/src/components/Dashboard.test.js
@@ -1,9 +1,36 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import Dashboard from './Dashboard';
+import { render } from '@testing-library/react';
+import Controls from './Dashboard';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Dashboard />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('<Controls />', () => {
+    it('renders without crashing', ()=> {
+        render(<Controls />)
+    })
+    it('recognizes the strike button', () =>{
+        const { getByText } = render(<Controls />)
+        getByText(/strike/);
+    })
+})
+
+
+
+
+
+// describe('<Display />', () => {
+//     it('renders without crashing', () => {
+//         render(<Display />)
+//     });
+//     it('has props that work', () => {
+//         const balls = 3, strikes = 2;
+//         const display = render(<Display strikes={strikes} balls={balls} />)
+//         //creating dynamic regular expressions
+//         display.getByText(new RegExp(`Strikes: ${strikes}`, 'i'))
+//         display.getByText(new RegExp(`Balls: ${balls}`, 'i'))
+//     });
+//     //edge case to clarify specific behavior (want dumb components)
+//     it('handles edge cases', () => {
+//         const display = render(<Display strikes={200} balls={-10000000} />)
+//         display.getByText(new RegExp(/Strikes: 200/))
+//         display.getByText(new RegExp(/Balls: -10000000/))
+//     })
+// })
